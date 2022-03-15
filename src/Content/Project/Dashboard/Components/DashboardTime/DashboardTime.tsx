1a3b33a2d7f15ef2778188bfdd0bb7b1d874bc26
Phil <phillippelliott@gmail.com>
2022-03-15 08:17:49 -0400

    fixed design

--- src/Content/Project/Dashboard/Components/DashboardTime/DashboardTime.tsx
+++ src/Content/Project/Dashboard/Components/DashboardTime/DashboardTime.tsx
@@ -1,16 +1,16 @@
 import React from "react"
 import "./DashboardTime.scss"
+import DashboardDeadlines from "../DashboardDeadlines/DashboardDeadlines"
+import { FaFlagCheckered } from "react-icons/fa"
 
 const DashboardTime = () => {
   return (
     <div className="dashboard-time-container">
-      <div className="dashboard-time-left">
-        <h1>Overall Progress</h1>
-      </div>
+      <DashboardDeadlines />
       <div className="dashboard-time-right">
-        <h1>Product Launch Date</h1>
-        <div>
-          <p>Img</p>
+        <h1>Projected Launch Date</h1>
+        <div className="dashboard-time-content">
+          <FaFlagCheckered className="flag-icon" />
           <div>
             <p>182 Days</p>
             <p>Friday, December 15</p>
@@ -24,6 +24,14 @@ const DashboardTime = () => {
 export default DashboardTime
 
 /*
+
+  copy members section from ms planner 
+  make everything responsive 
+  make add project a modal 
+    create logo, name, description, milestones, features 
+  need a delete bttn on project in project hub 
+
+
   left section 
     - overall progress
       do half a doughnut graph with a meter going the percentage of tasks done 
