67f830d302fd059ea640bcbe48adb4390f69ccaf
Phil <phillippelliott@gmail.com>
2022-12-01 12:08:34 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from "react"
-import "./App.scss"
-import Dashboard from "./Dashboard/Dashboard"
-import SignIn from "./SignIn/SignIn"
+import React from "react";
+import "./App.scss";
+import Dashboard from "./Dashboard/Dashboard";
+import SignIn from "./SignIn/SignIn";
 
 const App = () => {
   return (
     <div>
       {/* <Dashboard /> */}
       <SignIn />
+      <h1>Hello</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
