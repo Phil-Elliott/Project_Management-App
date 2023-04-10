233f384a0bb79be4366a31ca8c05dd4d245ca759
Phil <phillippelliott@gmail.com>
2023-04-10 12:30:42 +0700

    finished top section

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -3,11 +3,20 @@ import styles from "./LandingPage.module.scss";
 import Header from "./Header/Header";
 import TopSection from "./TopSection/TopSection";
 import Board3 from "./../../public/Board3.gif";
+import hero from "./../../public/hero2.jpg";
 
 const LandingPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.top}>
+      <div
+        className={styles.top}
+        style={{
+          backgroundImage: `url(${hero})`,
+          // backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <Header />
         <TopSection />
       </div>
